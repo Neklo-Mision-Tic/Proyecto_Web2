b2420da1dc96a05dcc91647af00efd9065264699
Osbaldo <oscolo79@gmail.com>
2021-12-19 12:26:19 -0500

    Modificaciónb AuthLayout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -6,7 +6,7 @@ const AuthLayaout =()=>{
         <div className='flex flex-col md:flex-row flex-no-wrap h-screen'>
       <div className='flex w-full h-full'>
         <div className='w-full h-full  overflow-y-scroll'>
-          Layout de Autenticacion Desplegado
+          Proyecto Neklo.
           <Outlet />
         </div>
       </div>
