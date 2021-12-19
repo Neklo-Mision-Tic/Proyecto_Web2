bc9f9ffa0997fb1d0165a2c4de549e14a26ce562
Osbaldo <oscolo79@gmail.com>
2021-12-19 17:39:00 -0500

    Actualizaciones login

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -6,13 +6,16 @@ const AuthLayaout =()=>{
         <div className='flex flex-col md:flex-row flex-no-wrap h-screen'>
       <div className='flex w-full h-full'>
         <div className='w-full h-full  overflow-y-scroll'>
-          Proyecto Neklo.
+          
+
+          
           <Outlet />
         </div>
         
       </div>
       
     </div>
+    
     
     )
     
