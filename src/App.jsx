3cc06bd8d2618bf333124679c98a8a16626ea150
Nicolas Chavez <90515899+GitJN-1@users.noreply.github.com>
2021-12-19 10:58:56 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,131 @@
+import React, { useState,useEffect } from 'react';
+import PrivateLayout from 'layouts/PrivateLayout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserContext } from 'context/userContext';
+import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+import Index from 'pages/Index';
+import IndexProyectos from 'pages/proyectos/Index';
+import Admin from 'pages/Admin';
+import IndexUsuarios from 'pages/usuarios';
+import 'styles/globals.css';
+import 'styles/tabla.css'
+import EditarUsuario from 'pages/usuarios/editar';
+import AuthLayaout from 'layouts/AuthLayout';
+import Register from 'pages/auth/register';
+import Login from 'pages/auth/login';
+import { AuthContext } from 'context/authContext';
+import jwt_decode from 'jwt-decode';
+import EditarEstadoUsuario from 'pages/usuarios/editarEstado';
+import { CrearProyecto } from 'pages/proyectos/crearProyecto';
+import { IndexEstudiante } from 'pages/estudiantes/indexEstudiantes';
+import { CrearObjetivo } from 'pages/proyectos/crearObjetivo';
+import { EditarProyectos } from 'pages/proyectos/editarProyectos';
+import { IndexProyectosPublic } from 'pages/proyectosPublic';
+import { EditarProyectoPublico } from 'pages/proyectosPublic/editarPyectoPublico';
+import { ProyectosByLider } from 'pages/proyectosPublic/proyectosByLider';
+import { CrearObjetivoLider } from 'pages/proyectosPublic/crearObjetivoLider';
+import { CrearProyectoLider } from 'pages/proyectosPublic/crearProyectoLider';
+import { IndexInscripcion } from 'pages/inscripcion/indexInscripcion';
+
+// import PrivateRoute from 'components/PrivateRoute';
+// const httplink=createHttpLink({
+//   uri:"https://servidor-gql-proyectos.herokuapp.com/graphql"
+// })
+
+const httplink=createHttpLink({
+    //uri:"http://localhost:4000/graphql",
+    uri:"https://servidor-gql-proyectos.herokuapp.com/graphql"
+    
+   });
+  
+   const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = JSON.parse(localStorage.getItem('token'));
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+const client=new ApolloClient({
+  //uri:"https://servidor-gql-proyectos.herokuapp.com/graphql",
+  cache:new InMemoryCache(),
+  link:authLink.concat(httplink),
+  
+});
+function App() {
+  const [userData, setUserData] = useState({});
+  const [authToken, setAuthToken]=useState('');
+
+  const setToken=(token)=>{
+      setAuthToken(token)
+      if (token){
+        localStorage.setItem('token',JSON.stringify(token));
+      }
+      else{
+        localStorage.removeItem('token');
+      }
+  }
+
+
+   useEffect(() => {
+ 
+    if(authToken){
+      const decoded=jwt_decode(authToken);
+      setUserData({
+        _id:decoded._id,
+        nombre:decoded.nombre,
+        apellido:decoded.apellido,
+        identificacion:decoded.identificacion,
+        correo:decoded.correo,
+        rol:decoded.rol
+      });
+      console.log('EL DECODIVFICADOXXXXXXXXXXXXXXXXXXXXXXXX', decoded);
+    }
+   }, [authToken]);
+
+  return (
+    <ApolloProvider client ={client}>
+    
+    <AuthContext.Provider value={{authToken,setAuthToken,setToken}}>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<PrivateLayout />}>
+              <Route path='' element={<Index />} />
+              <Route path='/usuarios' element={<IndexUsuarios />} />
+              <Route path='/usuarios/editar/:_id' element={<EditarUsuario />} />
+              <Route path='/usuarios/editarEstado/:_id' element={<EditarEstadoUsuario />} />
+              <Route path='admin' element={<Admin />} />
+              <Route path='admin/proyectos' element={<IndexProyectos/>}/>
+              <Route path='/proyectos/crear' element={<CrearProyecto/>}/>
+              <Route path='/proyectos/crearObjetivo/:_id' element={<CrearObjetivo/>}/>
+              <Route path='/proyectos/editarProyecto/:_id' element={<EditarProyectos/>}/>
+              <Route path='admin/usuarios' element={<IndexUsuarios />} />
+              <Route path='estudiantes' element={<IndexEstudiante/>}/>
+              <Route path='allProjects' element={<IndexProyectosPublic/>}/>
+              <Route path='/allProjects/liderEditarProyecto/:_id' element={<EditarProyectoPublico/>}/>
+              <Route path='/proyectosByLider/:_id' element={<ProyectosByLider/>}/>
+              <Route path='/proyectosByLider/crearObjetivoLider/:_id' element={<CrearObjetivoLider/>}/>
+              <Route path='/proyectosByLider/crearProyectoLider/:_id' element={<CrearProyectoLider/>}/>
+              <Route path='/inscripciones' element={<IndexInscripcion/>}/>
+            </Route>
+
+            <Route path="/auth" element={<AuthLayaout/>}>
+              <Route path='register' element={<Register/>}/>
+              <Route path='login' element={<Login/>}/>
+            </Route>
+          </Routes>
+          
+        </BrowserRouter>
+      </UserContext.Provider>
+      </AuthContext.Provider>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ src/graphql/proyectos/mutations.js
@@ -0,0 +1,46 @@
+import {gql} from '@apollo/client';
+
+const CREAR_PROYECTO=gql`
+mutation CrearProyecto($nombre: String!, $presupuesto: Float!, $fechaInicio: Date!, $fechaFin: Date!, $estado: Enum_EstadoProyecto, $fase: Enum_FaseProyecto, $lider: String!, $objetivos: [crearObjetivo]) {
+    crearProyecto(nombre: $nombre, presupuesto: $presupuesto, fechaInicio: $fechaInicio, fechaFin: $fechaFin, estado: $estado, fase: $fase, lider: $lider, objetivos: $objetivos) {
+      nombre
+      _id
+      presupuesto
+      fechaInicio
+      fechaFin
+      estado
+      fase
+      lider {
+        _id
+      }
+      objetivos {
+        _id
+        tipo
+      }
+    }
+  }
+  `
+  const CREAR_OBJETIVO=gql`
+  mutation CrearObjetivo($idProyecto: String!, $descripcion: String!, $tipo: Enum_TipoObjetivo!) {
+    crearObjetivo(idProyecto: $idProyecto, descripcion: $descripcion, tipo: $tipo) {
+      nombre
+    }
+  }
+  `
+  const EDITAR_PROYECTO=gql`
+  mutation ModificarProyecto($_id: String!, $estado: Enum_EstadoProyecto, $nombre: String, $fase: Enum_FaseProyecto, $presupuesto: Float,$fechaInicio: Date, $fechaFin: Date, $lider: String) {
+    modificarProyecto(_id: $_id, estado: $estado, nombre: $nombre, fase: $fase, presupuesto: $presupuesto,fechaInicio: $fechaInicio, fechaFin: $fechaFin, lider: $lider) {
+      estado
+      nombre
+      fase
+      presupuesto
+      fechaInicio
+      fechaFin
+      lider {
+        _id
+        nombre
+      }
+    }
+  }
+  `
+  export {CREAR_PROYECTO,CREAR_OBJETIVO,EDITAR_PROYECTO};
