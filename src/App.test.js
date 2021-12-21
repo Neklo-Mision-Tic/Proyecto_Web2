586035335f81b5a3ee72ad783be59b7c452c193e
Osbaldo <oscolo79@gmail.com>
2021-12-20 19:10:17 -0500

    Actualizaciones test4

--- src/App.test.js
+++ src/App.test.js
@@ -12,18 +12,18 @@ it('renders not authorized if the roles dont match', () => {
       </PrivateRouter>
     </UserContext.Provider>
   );
-//   expect(screen.getByTestId('not-authorized')).toHaveTextContent(
-//     'No estás autorizado para ver este sitio.'
-//   );
-// });
-
-it('renders the children if the user role is in the roleList', () => {
-  render(
-    <UserContext.Provider value={{ userData: { rol: 'ADMINISTRADOR' } }}>
-      <PrivateRouter roleList={['ADMINISTRADOR']}>
-        <div data-testid='children'>Este es el children</div>
-      </PrivateRouter>
-    </UserContext.Provider>
+  expect(screen.getByTestId('not-authorized')).toHaveTextContent(
+    'No estás autorizado para ver este sitio.'
   );
-  expect(screen.getByTestId('children')).toBeInTheDocument();
 });
+
+// it('renders the children if the user role is in the roleList', () => {
+//   render(
+//     <UserContext.Provider value={{ userData: { rol: 'ADMINISTRADOR' } }}>
+//       <PrivateRouter roleList={['ADMINISTRADOR']}>
+//         <div data-testid='children'>Este es el children</div>
+//       </PrivateRouter>
+//     </UserContext.Provider>
+//   );
+//   expect(screen.getByTestId('children')).toBeInTheDocument();
+// });
